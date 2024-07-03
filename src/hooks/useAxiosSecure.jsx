08c64e3b8aa91b33bcd1959041b96c7898b2fbcd
faisal-shohag <faisalshohagprog@gmail.com>
2024-07-03 16:51:19 +0600

    updated

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -2,12 +2,12 @@ import axios from "axios";
 
 
 const axiosSecure = axios.create({
-    //baseURL: "https://student-management-server-soft.vercel.app/",
-     baseURL: "http://192.168.0.102:3000/",
+    baseURL: "https://student-management-server-soft.vercel.app/",
+    //  baseURL: "http://192.168.0.102:3000/",
     headers: {
         'content-type': 'application/json'
     },
-    withCredentials: true
+    // withCredentials: true
 })
 // console.log(import.meta.env.VITE_serverKey)
 
